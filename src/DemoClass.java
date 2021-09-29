9641566cfa5f3f2ddddb4a8fdb68014a67ff9e47
60manojkumar@gmail.com <Motivated@2019>
2021-09-29 10:59:32 +0530

    Added Demo Class with print statements

---
+++ src/DemoClass.java
@@ -0,0 +1,11 @@
+public class DemoClass {
+
+public static void main (String args[]){
+
+ System.out.println("This is my first java program");
+ System.out.println("This is second line ");
+ System.out.println("This is third line ");
+
+}
+
+}
