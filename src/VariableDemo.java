f1da49baab42d9e74c6aee52108e9fcf292ba0f3
60manojkumar@gmail.com <Motivated@2019>
2021-09-30 10:59:03 +0530

    Variable Demo

---
+++ src/VariableDemo.java
@@ -0,0 +1,33 @@
+public class VariableDemo {
+
+    public static void main(String[] args) {
+
+        // SYNTAX : Datatype variable_name= value ;
+
+        String city = "AGRA"; //String value will always in " "
+
+        System.out.println("Printing variables " +city);
+
+        String country="INDIA";
+
+        System.out.println("Printing Country " +country);
+        String name="Java";
+
+        System.out.println("Printing name " +name);
+
+
+        int age=25;
+
+        System.out.println("Age is " +age);
+        int marks =75;
+        System.out.println("Marks is " +marks);
+
+       double salary=3214.5;
+
+        System.out.println("Salary is " +salary);
+        double price=3214.9;
+
+        System.out.println("Price is " +price);
+
+    }
+}
