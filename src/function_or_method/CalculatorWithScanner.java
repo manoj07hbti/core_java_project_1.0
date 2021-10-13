4a2a6d8abdcb66b9f8430cfc5ab05631e7e7675f
60manojkumar@gmail.com <Motivated@2019>
2021-10-13 10:46:59 +0530

    Scanner with Calculator

--- src/function_or_method/CalculatorWithScanner.java
+++ src/function_or_method/CalculatorWithScanner.java
@@ -11,19 +11,62 @@ public class CalculatorWithScanner {
         return sum;
     }
 
+    public int subtraction(int a, int b){
+
+        int sub=a-b;
+
+        return sub;
+    }
+
+    public int multi(int a, int b){
+
+        int mult=a*b;
+
+        return mult;
+    }
+
+    public int divide(int a, int b){
+
+        int div=a/b;
+
+        return div;
+    }
 
     public static void main(String[] args) {
 
         CalculatorWithScanner obj= new CalculatorWithScanner();
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+
+        System.out.println("Please enter Choice value of 1 for Addition, 2 for Subtraction, 3 for Multiplication and 4 for divide: ");
+        int choice=myObj.nextInt();
+
         System.out.println("Please enter value of a: ");
         int a=myObj.nextInt();
         System.out.println("Please enter value of b: ");
         int b=myObj.nextInt();
 
-       int result = obj.addition(a,b);
+        int result;
+    if (choice==1){
+        System.out.println("You have selected ADDITION ");
+       result= obj.addition(a,b);
+    }
+    else if(choice==2)
+    {
+        System.out.println("You have selected SUBTRACTION ");
+        result=obj.subtraction(a,b);
+    }
+    else if(choice==3){
+        System.out.println("You have selected MULTIPLICATION ");
+        result=obj.multi(a,b);
+    }
+    else {
+        System.out.println("You have selected DIVIDE ");
+        result= obj.divide(a,b);
+    }
 
-       System.out.println("Addition is "+result);
+     System.out.println("Calculator result is " +result);
+
+
     }
 
 }
