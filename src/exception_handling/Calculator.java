35077566dc908ef3b4551dbfc2907c365b9f99af
60manojkumar@gmail.com <Motivated@2019>
2021-10-26 08:49:43 +0530

    Exception with finally, throws and throw

--- src/exception_handling/Calculator.java
+++ src/exception_handling/Calculator.java
@@ -1,5 +1,7 @@
 package exception_handling;
 
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+
 public class Calculator {
 
     public void divide(int a, int b ){
@@ -12,13 +14,43 @@ public class Calculator {
 
             System.out.println("Exception occured "+e);
         }
-
+        finally {
+            System.out.println("This is finally block and it is going to execute every time ");
+        }
 
         System.out.println("Divide is : "+result);
     }
 
+    public void divideWithThrows (int a, int b) throws ArithmeticException {
+
+        int result=a/b;
+        System.out.println("inside divideWithThrows : "+ result);
+    }
+
+    public void addition(int a, int b ) throws Exception {
+
+        int result=a+b;
+        throw new Exception();
+    }
+
     public static void main(String[] args) {
         Calculator obj= new Calculator();
-        obj.divide(8,0);
+        obj.divide(8,2);
+
+        try {
+            obj.divideWithThrows(6, 0);
+        }
+        catch (Exception e){
+
+            System.out.println("Exception Occured ");
+        }
+
+
+        try {
+            obj.addition(2,4);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
